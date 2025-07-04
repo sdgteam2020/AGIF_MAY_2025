6e8537ffe455ac054e1d85871068bfa0ec81dc36
shivam bansal <shivam21bansal@gmail.com>
2025-07-04 11:56:42 +0530

    new model added

---
+++ DataAccessLayer/Migrations/20250703094312_New column Ioarmy added.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewcolumnIoarmyadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IOArmyNo",
+                table: "trnClaim",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMergePdf",
+                table: "trnClaim",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IOArmyNo",
+                table: "trnClaim");
+
+            migrationBuilder.DropColumn(
+                name: "IsMergePdf",
+                table: "trnClaim");
+        }
+    }
+}
