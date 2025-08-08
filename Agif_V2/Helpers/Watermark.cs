a3e4a78195ac26523a58da3419e28757a6563e96
Ankit <ankits6270@gmail.com>
2025-08-08 16:04:43 +0530

    commit

--- Agif_V2/Helpers/Watermark.cs
+++ Agif_V2/Helpers/Watermark.cs
@@ -57,35 +57,35 @@ namespace Agif_V2.Helpers
             }
             pdfReader.Close();
         }
-        //public void OpenPdf1(string ipAddress, string wwwRootPath)
-        //{
-        //    string tempOutputPath = Path.Combine(Path.GetDirectoryName(wwwRootPath), "temp_" + Path.GetFileName(wwwRootPath));
+        public void OpenPdf1(string ipAddress, string wwwRootPath)
+        {
+            string tempOutputPath = Path.Combine(Path.GetDirectoryName(wwwRootPath), "temp_" + Path.GetFileName(wwwRootPath));
 
-        //    using (var reader = new PdfReader(wwwRootPath))
-        //    using (var writer = new PdfWriter(tempOutputPath))
-        //    using (var pdfDoc = new PdfDocument(reader, writer))
-        //    {
-        //        var font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
-        //        string ipDisplay = string.IsNullOrEmpty(ipAddress) ? "IP Address Not Found!" : ipAddress.Replace(" ", "\u00A0");
-        //        string dateTimeDisplay = DateTime.Now.ToString("dd/MM/yyyy HH:mm").Replace(" ", "\u00A0");
+            using (var reader = new PdfReader(wwwRootPath))
+            using (var writer = new PdfWriter(tempOutputPath))
+            using (var pdfDoc = new PdfDocument(reader, writer))
+            {
+                var font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+                string ipDisplay = string.IsNullOrEmpty(ipAddress) ? "IP Address Not Found!" : ipAddress.Replace(" ", "\u00A0");
+                string dateTimeDisplay = DateTime.Now.ToString("dd/MM/yyyy HH:mm").Replace(" ", "\u00A0");
 
-        //        for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
-        //        {
-        //            PdfPage page = pdfDoc.GetPage(i);
-        //            var annotation = new PdfFreeTextAnnotation(
-        //                new iText.Kernel.Geom.Rectangle(100, 100, 400, 100),
-        //                new PdfString($"{ipDisplay}\n{dateTimeDisplay}"))
-        //                .SetDefaultAppearance(new PdfString("/Helv 12 Tf 0.5 g"))
-        //                .SetColor(ColorConstants.LIGHT_GRAY)
-        //                .SetFlags(PdfAnnotation.PRINT);
+                for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+                {
+                    PdfPage page = pdfDoc.GetPage(i);
+                    var annotation = new PdfFreeTextAnnotation(
+                        new iText.Kernel.Geom.Rectangle(100, 100, 400, 100),
+                        new PdfString($"{ipDisplay}\n{dateTimeDisplay}"))
+                        .SetDefaultAppearance(new PdfString("/Helv 12 Tf 0.5 g"))
+                        .SetColor(ColorConstants.LIGHT_GRAY)
+                        .SetFlags(PdfAnnotation.PRINT);
 
-        //            page.AddAnnotation(annotation);
-        //        }
-        //    }
-        //    // Now the file is released, safe to overwrite
-        //    File.Copy(tempOutputPath, wwwRootPath, overwrite: true);
-        //    File.Delete(tempOutputPath);
-        //}
+                    page.AddAnnotation(annotation);
+                }
+            }
+            // Now the file is released, safe to overwrite
+            File.Copy(tempOutputPath, wwwRootPath, overwrite: true);
+            File.Delete(tempOutputPath);
+        }
 
 
     }
