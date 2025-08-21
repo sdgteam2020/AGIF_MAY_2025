20fab587e3ea5d756445d170685e27a5566c8edc
shivam bansal <shivam21bansal@gmail.com>
2025-08-21 12:54:00 +0530

    commit changes

--- Agif_V2/wwwroot/DevJs/MaturityLoantype.js
+++ Agif_V2/wwwroot/DevJs/MaturityLoantype.js
@@ -3,16 +3,23 @@
 $(document).ready(function () {
     mMsater(0, "Category", 6, 0);
     mMsater(0, "PurposeOfWithdrwal", 19, 0);
+
+    $('#btnAgree').click(function () {
+        const PurposeOfWithdrwal = $('#PurposeOfWithdrwal').val();
+        const applicantCategory = $('#Category').val();
+
+        if (PurposeOfWithdrwal !== "" && applicantCategory !== "") {
+            document.getElementById('loanForm').submit(); // Submit the form
+        } else {
+            $('.errormessage').html('<div class="alert alert-danger" role="alert">⚠️ Please Select Category and Withdrawal Purpose Both!</div>');
+
+        }
+
+    })
+
+
+    $('#genInstr').on('click', function (e) {
+        e.preventDefault();
+        window.open(`/PdfViewer/InstrPCA`, '_blank');
+    });
 });
-$('#btnAgree').click(function () {
-    const PurposeOfWithdrwal = $('#PurposeOfWithdrwal').val();
-    const applicantCategory = $('#Category').val();
-
-    if (PurposeOfWithdrwal !== "" && applicantCategory !== "") {
-        document.getElementById('loanForm').submit(); // Submit the form
-    } else {
-        $('.errormessage').html('<div class="alert alert-danger" role="alert">⚠️ Please Select Category and Withdrawal Purpose Both!</div>');
-
-    }
-
-})
